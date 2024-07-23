cf2b5a1efa065f69cd25528b07378568ba55bb55
di.coder <175606545+di-code-star@users.noreply.github.com>
2024-07-23 20:19:03 +0500

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
       </Header>
       <main className=''>
         <section className="container mx-auto p-4 flex justify-center items-center h-1/2">
-          <div className="max-w-md w-full bg-white shadow-lg rounded p-4 border-2 border-gray-200">
+          <div className="max-w-md w-full bg-white shadow-lg rounded-lg p-4 border-2 border-gray-200">
             <h2 className="text-3xl font-bold mb-4 text-center">Enter Pizza Details</h2>
             <form id="pizza-form" className='text-2xl'>
 
@@ -77,7 +77,7 @@ function App() {
               <input type="number" id="slices" className="w-full px-3 py-2 border border-gray-300 rounded" placeholder="8 slices of 14 inches pizza"
                 onChange={(e) => { setNoOfSlices(e.target.value) }} />
 
-              <button id="analyze-btn" className="bg-gradient-to-tl from-red-700 to-blue-600 hover:bg-red-800 text-white font-bold my-2 py-2 px-4 rounded w-full"
+              <button id="analyze-btn" className="bg-gradient-to-tl from-red-700 to-blue-600 hover:text-black text-white font-bold my-2 py-2 px-4 rounded w-full"
                 onClick={handleSubmit}>
                 Analyze Pizza
               </button>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,9 +2,9 @@
 const Header = () => {
     return (
         <header>
-            <nav className="bg-gradient-to-tl from-red-700 to-blue-600 hover:bg-red-800 py-4 text-lg font-bold">
+            <nav className="bg-gray-300 py-4 text-lg font-bold">
                 <div className="container mx-auto px-4 flex justify-between">
-                    <h1 className="text-white text-2xl">Pizza Analyzer</h1>
+                    <h1 className="text-black text-2xl">Pizza Analyzer</h1>
                     {/* <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded" id="back-btn">
                         Back
                     </button> */}
